10b932f1d304707bed4926eecab30cdabce70c8f
Jiyun Yang <ji.yang@samsung.com>
2019-05-29 16:58:08 +0900

    Introduce Evas-plugin
    Evas-plugin is used by EFL applications that wish to use Dali.
    It provides a means for initializing the resources required by the Dali::Core.
    
    Change-Id: I2b4428615e42771f94ee1df3c818386e77349c6f
    Signed-off-by: Jiyun Yang <ji.yang@samsung.com>

---
+++ dali-extension/internal/evas-plugin/evas-event-handler.h
@@ -0,0 +1,219 @@
+#ifndef __DALI_EXTENSION_INTERNAL_EVAS_EVENT_HANDLER__
+#define __DALI_EXTENSION_INTERNAL_EVAS_EVENT_HANDLER__
+
+/*
+ * Copyright ( c ) 2019 Samsung Electronics Co., Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 ( the "License" );
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+// EXTERNAL INCLUDES
+#include <Evas.h>
+#include <Elementary.h>
+#include <Ecore_Wl2.h>
+#include <Ecore_Input.h>
+#include <Ecore_IMF.h>
+#include <Ecore_IMF_Evas.h>
+
+#include <string>
+#include <vector>
+
+// INTERNAL INCLUDES
+
+namespace Dali
+{
+
+namespace Extension
+{
+
+namespace Internal
+{
+
+class EvasPluginEventInterface;
+
+class EvasPluginEventHandler
+{
+public:
+  /**
+   * @brief Constructor
+   *
+   * @param[in] evasPluginEventInterface Used to send event to evas plugin
+   */
+  EvasPluginEventHandler( EvasPluginEventInterface& evasPluginEventInterface );
+
+  /**
+   * Destructor.
+   */
+  ~EvasPluginEventHandler();
+
+public:
+
+  EvasPluginEventInterface& GetEvasPluginInterface()
+  {
+    return mEvasPluginEventInterface;
+  }
+
+private:
+
+  void EnableEcoreWl2Events();
+
+  void DisableEcoreWl2Events();
+
+  /////////////////////////////////////////////////////////////////////////////////////////////////////
+  // Event callbacks
+  /////////////////////////////////////////////////////////////////////////////////////////////////////
+
+  /**
+   * @brief Called when the mouse down is received
+   */
+  static void OnEvasObjectMouseDown( void *data, Evas* evas, Evas_Object* evasObject, void* event );
+
+  /**
+   * @brief Called when the mouse up is received
+   */
+  static void OnEvasObjectMouseUp( void *data, Evas* evas, Evas_Object* evasObject, void* event );
+
+  /**
+   * @brief Called when the mouse move is received
+   */
+  static void OnEvasObjectMouseMove( void *data, Evas* evas, Evas_Object* evasObject, void* event );
+
+  /**
+   * @brief Called when the mouse wheel is received
+   */
+  static void OnEvasObjectMouseWheel( void *data, Evas* evas, Evas_Object* evasObject, void* event );
+
+  /**
+   * @brief Called when the multi-touch down is received
+   */
+  static void OnEvasObjectMultiTouchDown( void *data, Evas* evas, Evas_Object* evasObject, void* event );
+
+  /**
+   * @brief Called when the multi-touch up is received
+   */
+  static void OnEvasObjectMultiTouchUp( void *data, Evas* evas, Evas_Object* evasObject, void* event );
+
+  /**
+   * @brief Called when the multi-touch move is received
+   */
+  static void OnEvasObjectMultiTouchMove( void *data, Evas* evas, Evas_Object* evasObject, void* event );
+
+  /**
+   * @brief Called when key down is received
+   */
+  static void OnEvasObjectKeyDown( void *data, Evas* evas, Evas_Object* evasObject, void* event );
+
+  /**
+   * @brief Called when key up is received
+   */
+  static void OnEvasObjectKeyUp( void *data, Evas* evas, Evas_Object* evasObject, void* event );
+
+  /////////////////////////////////////////////////////////////////////////////////////////////////////
+  // Geometry callbacks
+  /////////////////////////////////////////////////////////////////////////////////////////////////////
+
+  /**
+   * @brief Called when move is received
+   */
+  static void OnEvasObjectMove( void *data, Evas* evas, Evas_Object* evasObject, void* event );
+
+  /**
+   * @brief Called when resize is received
+   */
+  static void OnEvasObjectResize( void *data, Evas* evas, Evas_Object* evasObject, void* event );
+
+  /////////////////////////////////////////////////////////////////////////////////////////////////////
+  // Focus callbacks
+  /////////////////////////////////////////////////////////////////////////////////////////////////////
+
+  /**
+   * @brief Called when evas object focus in is received
+   */
+  static void OnEvasObjectFocusIn( void *data, Evas* evas, Evas_Object* evasObject, void* event );
+
+  /**
+   * @brief Called when evas object focus out is received
+   */
+  static void OnEvasObjectFocusOut( void *data, Evas* evas, Evas_Object* evasObject, void* event );
+
+  /**
+   * @brief Called when evas focus in is received
+   */
+  static void OnEvasFocusIn( void *data, Evas* evas, void* event );
+
+  /**
+   * @brief Called when evas focus out is received
+   */
+  static void OnEvasFocusOut( void *data, Evas* evas, void* event );
+
+  /////////////////////////////////////////////////////////////////////////////////////////////////////
+  // Render callbacks
+  /////////////////////////////////////////////////////////////////////////////////////////////////////
+
+  /**
+   * @brief Called when evas render post
+   */
+  static void OnEvasRenderPost( void *data, Evas* evas, void* event );
+
+  /////////////////////////////////////////////////////////////////////////////////////////////////////
+  // Elm Access callbacks
+  /////////////////////////////////////////////////////////////////////////////////////////////////////
+
+  static Eina_Bool OnElmAccessActionHighlight( void* data, Evas_Object* evasObject, Elm_Access_Action_Info* info );
+
+  static Eina_Bool OnElmAccessActionUnhighlight( void* data, Evas_Object* evasObject, Elm_Access_Action_Info* info );
+
+  static Eina_Bool OnElmAccessActionHighlightNext( void* data, Evas_Object* evasObject, Elm_Access_Action_Info* info );
+
+  static Eina_Bool OnElmAccessActionHighlightPrev( void* data, Evas_Object* evasObject, Elm_Access_Action_Info* info );
+
+  static Eina_Bool OnElmAccessActionActivate( void* data, Evas_Object* evasObject, Elm_Access_Action_Info* info );
+
+  static Eina_Bool OnElmAccessActionScroll( void* data, Evas_Object* evasObject, Elm_Access_Action_Info* info );
+
+  static Eina_Bool OnElmAccessActionUp( void* data, Evas_Object* evasObject, Elm_Access_Action_Info* info );
+
+  static Eina_Bool OnElmAccessActionDown( void* data, Evas_Object* evasObject, Elm_Access_Action_Info* info );
+
+  static Eina_Bool OnElmAccessActionBack( void* data, Evas_Object* evasObject, Elm_Access_Action_Info* info );
+
+  static Eina_Bool OnElmAccessActionRead( void* data, Evas_Object* evasObject, Elm_Access_Action_Info* info );
+
+  /////////////////////////////////////////////////////////////////////////////////////////////////////
+  // Elm Focus callbacks
+  /////////////////////////////////////////////////////////////////////////////////////////////////////
+
+  static void OnEvasObjectSmartFocused( void* data, Evas_Object* evasObject, void* event );
+
+  static void OnEvasObjectSmartUnfocused( void* data, Evas_Object* evasObject, void* event );
+
+  /////////////////////////////////////////////////////////////////////////////////////////////////////
+  // Ecore Wl2 callbacks
+  /////////////////////////////////////////////////////////////////////////////////////////////////////
+
+  static Eina_Bool OnEcoreWl2EventWindowVisibilityChange( void* data, int type, void* event );
+
+private:
+
+  EvasPluginEventInterface&         mEvasPluginEventInterface;
+  std::vector<Ecore_Event_Handler*> mEcoreEventHandlers;
+};
+
+}  // namespace Internal
+
+}  // namespace Extension
+
+}  // namespace Dali
+
+#endif
---
+++ dali-extension/internal/evas-plugin/evas-event-interface.h
@@ -0,0 +1,166 @@
+#ifndef __DALI_EXTENSION_INTERNAL_EVAS_EVENT_INTERFACE_H__
+#define __DALI_EXTENSION_INTERNAL_EVAS_EVENT_INTERFACE_H__
+
+/*
+ * Copyright ( c ) 2019 Samsung Electronics Co., Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 ( the "License" );
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+// EXTERNAL INCLUDES
+#include <dali/integration-api/events/point.h>
+#include <dali/integration-api/events/wheel-event-integ.h>
+#include <dali/integration-api/events/key-event-integ.h>
+
+namespace Dali
+{
+
+template <typename T>
+class Rect;
+
+struct TouchPoint;
+struct WheelEvent;
+struct KeyEvent;
+
+namespace Extension
+{
+
+namespace Internal
+{
+
+class EvasWrapper;
+
+enum AccessActionType
+{
+   ACCESS_ACTION_FIRST = -1,
+
+   ACCESS_ACTION_HIGHLIGHT,      /* highlight an object */
+
+   ACCESS_ACTION_UNHIGHLIGHT,    /* unhighlight an object */
+
+   ACCESS_ACTION_HIGHLIGHT_NEXT, /* set highlight to next object */
+
+   ACCESS_ACTION_HIGHLIGHT_PREV, /* set highlight to previous object */
+
+   ACCESS_ACTION_ACTIVATE,       /* activate a highlight object */
+
+   ACCESS_ACTION_SCROLL,         /* scroll if one of highlight object parents is scrollable */
+
+   ACCESS_ACTION_UP,             /* change value up of highlight object */
+
+   ACCESS_ACTION_DOWN,           /* change value down of highlight object */
+
+   ACCESS_ACTION_BACK,           /* go back to a previous view (ex: pop naviframe item) */
+
+   ACCESS_ACTION_READ,           /* highlight an object */
+
+   ACCESS_ACTION_LAST
+};
+
+struct AccessActionInfo
+{
+   int              x;
+   int              y;
+   unsigned int     mouseType; /* 0: mouse down
+                                  1: mouse move
+                                  2: mouse up   */
+   AccessActionType actionType;
+   AccessActionType actionBy;
+   bool             highlightCycle : 1;
+   unsigned int     timeStamp;
+};
+
+class EvasPluginEventInterface
+{
+public:
+   /**
+   * @brief Gets an EvasWrapper instance to connect events
+   *
+   * @return The EvasWrapper instance
+   */
+  virtual EvasWrapper* GetEvasWrapper() const = 0;
+
+  /**
+   * @brief Handle evas object event
+   *
+   * @param[in] touchPoint is the information of touch
+   * @param[in] timeStamp is that touch is occured
+   */
+  virtual void OnEvasObjectTouchEvent( Dali::Integration::Point& touchPoint, unsigned long timeStamp ) = 0;
+
+  /**
+   * @brief Handle evas object event
+   *
+   * @param[in] wheelEvent is the information of wheel
+   */
+  virtual void OnEvasObjectWheelEvent( Dali::Integration::WheelEvent& wheelEvent ) = 0;
+
+  /**
+   * @brief Handle evas object event
+   *
+   * @param[in] keyEvent is the information of key
+   */
+  virtual void OnEvasObjectKeyEvent( Dali::Integration::KeyEvent& keyEvent ) = 0;
+
+  /**
+   * @brief Handle evas object geometry
+   *
+   * @param geometry is the move information of evas object
+   */
+  virtual void OnEvasObjectMove( const Rect<int>& geometry ) = 0;
+
+  /**
+   * @brief Handle evas object geometry
+   *
+   * @param geometry is the resize information of evas object
+   */
+  virtual void OnEvasObjectResize( const Rect<int>& geometry ) = 0;
+
+  /**
+   * @brief Handle evas object focus in
+   */
+  virtual void OnEvasObjectFocusIn() = 0;
+
+  /**
+   * @brief Handle evas object focus out
+   */
+  virtual void OnEvasObjectFocusOut() = 0;
+
+  /**
+   * @brief Handle evas object render post
+   */
+  virtual void OnEvasPostRender() = 0;
+
+   /**
+   * @brief Called when the accessibility action event dispatched from elm_access.
+   * @param[in] accessActionInfo elm accessibility action information structure
+   * @return True if the event was handled
+   */
+  virtual bool OnElmAccessibilityActionEvent( AccessActionInfo& accessActionInfo ) = 0;
+
+  /**
+   * @brief Process the ecore wayland visibility.
+   *
+   * @param[in] visibility True is that ecore wayland window is show up and false is not
+   */
+  virtual void OnEcoreWl2VisibilityChange( bool visibility ) = 0;
+};
+
+}  // namespace Internal
+
+}  // namespace Extension
+
+}  // namespace Dali
+
+#endif
