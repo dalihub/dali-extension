f685af8331b39c70a5e270e9dae19d169007adb5
Heeyong Song <heeyong.song@samsung.com>
2019-11-25 13:44:44 +0900

    (Vector) Add Finalize method
    - Ensure some member variables are deleted in the main thread
    
    Change-Id: I6211a69829198c3e77155352fa149da391205593

--- dali-extension/vector-animation-renderer/tizen-vector-animation-renderer.cpp
+++ dali-extension/vector-animation-renderer/tizen-vector-animation-renderer.cpp
@@ -79,8 +79,6 @@ TizenVectorAnimationRenderer::~TizenVectorAnimationRenderer()
   Dali::Mutex::ScopedLock lock( mMutex );
 
   ResetBuffers();
-
-  TizenVectorAnimationManager::Get().RemoveEventHandler( *this );
 }
 
 bool TizenVectorAnimationRenderer::Initialize( const std::string& url )
@@ -107,6 +105,21 @@ bool TizenVectorAnimationRenderer::Initialize( const std::string& url )
   DALI_LOG_RELEASE_INFO( "TizenVectorAnimationRenderer::Initialize: file [%s] [%p]\n", url.c_str(), this );
 
   return true;
+}
+
+void TizenVectorAnimationRenderer::Finalize()
+{
+  Dali::Mutex::ScopedLock lock( mMutex );
+
+  TizenVectorAnimationManager::Get().RemoveEventHandler( *this );
+
+  mRenderer.Reset();
+  mTexture.Reset();
+  mRenderedTexture.Reset();
+  mVectorRenderer.reset();
+
+  mTargetSurface = nullptr;
+  mTbmQueue = NULL;
 }
 
 void TizenVectorAnimationRenderer::SetRenderer( Renderer renderer )
@@ -165,6 +178,11 @@ void TizenVectorAnimationRenderer::SetSize( uint32_t width, uint32_t height )
 bool TizenVectorAnimationRenderer::Render( uint32_t frameNumber )
 {
   Dali::Mutex::ScopedLock lock( mMutex );
+
+  if( !mTbmQueue || !mVectorRenderer )
+  {
+    return false;
+  }
 
   if( tbm_surface_queue_can_dequeue( mTbmQueue, 0 ) )
   {
@@ -260,14 +278,17 @@ void TizenVectorAnimationRenderer::GetLayerInfo( Property::Map& map ) const
 {
   Dali::Mutex::ScopedLock lock( mMutex );
 
-  auto layerInfo = mVectorRenderer->layers();
-
-  for( auto&& iter : layerInfo )
+  if( mVectorRenderer )
   {
-    Property::Array frames;
-    frames.PushBack( std::get< 1 >( iter ) );
-    frames.PushBack( std::get< 2 >( iter ) );
-    map.Add( std::get< 0 >( iter ), frames );
+    auto layerInfo = mVectorRenderer->layers();
+
+    for( auto&& iter : layerInfo )
+    {
+      Property::Array frames;
+      frames.PushBack( std::get< 1 >( iter ) );
+      frames.PushBack( std::get< 2 >( iter ) );
+      map.Add( std::get< 0 >( iter ), frames );
+    }
   }
 }
 
@@ -285,7 +306,7 @@ void TizenVectorAnimationRenderer::NotifyEvent()
     DALI_LOG_RELEASE_INFO( "TizenVectorAnimationRenderer::NotifyEvent: Set Texture [%p]\n", this );
 
     // Set texture
-    if( mRenderer )
+    if( mRenderer && mRenderedTexture )
     {
       TextureSet textureSet = mRenderer.GetTextures();
       textureSet.SetTexture( 0, mRenderedTexture );
