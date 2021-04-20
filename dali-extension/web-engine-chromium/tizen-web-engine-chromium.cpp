5be52190310147ceedf7b249b416bdf5b75439b0
Joogab Yun <joogab.yun@samsung.com>
2021-04-20 10:36:27 +0900

    Revert "[Tizen] Disable hover and wheel code until chromium support it"
    This reverts commit 7d7217e1562fbcdd0618ee9c3dc2a9b8cf49347e.

--- dali-extension/web-engine-chromium/tizen-web-engine-chromium.cpp
+++ dali-extension/web-engine-chromium/tizen-web-engine-chromium.cpp
@@ -593,7 +593,7 @@ public:
 
   bool SendHoverEvent(const HoverEvent& hover)
   {
-#if 0
+#if defined(OS_TIZEN_TV)
     //TODO...left/right/middle of mouse could not be acquired now.
     Ewk_Mouse_Button_Type type = EWK_Mouse_Button_Left;
     switch ( hover.GetState( 0 ) )
@@ -630,7 +630,7 @@ public:
 
   bool SendWheelEvent(const WheelEvent& wheel)
   {
-#if 0
+#if defined(OS_TIZEN_TV)
     Eina_Bool direction = wheel.GetDirection() ? true : false;
     int step = wheel.GetDelta();
     float x = wheel.GetPoint().x;
