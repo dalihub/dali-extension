fa5551bfaeb9ea0371cae1ce279a7ab495adcc90
huayong.xu <huayong.xu@samsung.com>
2021-08-17 10:21:10 +0800

    [Tizen] Fix a SVACE issue that evas_object_image_data_get need be checked.
    Change-Id: I0fda10a791b53a514c50472138427eb44a9d1d04

--- dali-extension/web-engine-chromium/tizen-web-engine-chromium.cpp
+++ dali-extension/web-engine-chromium/tizen-web-engine-chromium.cpp
@@ -253,14 +253,18 @@ public:
       return Dali::PixelData();
     }
 
+    // color-space is argb8888.
+    uint8_t* pixelBuffer = ( uint8_t* ) evas_object_image_data_get( iconObject, false );
+    if (!pixelBuffer)
+    {
+      return Dali::PixelData();
+    }
+
     int width = 0, height = 0;
     evas_object_image_size_get( iconObject, &width, &height );
 
     uint32_t bufferSize = width * height * 4;
     uint8_t* convertedBuffer = new uint8_t[ bufferSize ];
-
-    // color-space is argb8888.
-    uint8_t* pixelBuffer = ( uint8_t* ) evas_object_image_data_get( iconObject, false );
 
     // convert the color-space to rgba8888.
     for( uint32_t i = 0; i < bufferSize; i += 4 )
