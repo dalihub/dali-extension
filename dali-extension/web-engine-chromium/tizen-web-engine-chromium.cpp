1721e13c4cade91ac4878d29515fb05e27d57e13
Youngsun Suh <youngsun.suh@samsung.com>
2025-02-11 14:58:08 +0900

    [Tizen] Fix for UserMediaPermissionRequest object to be maintained by user
    This reverts commit e9c556570702c9e8f31e1748c7faf995be550b65.
    
    Change-Id: Idc8cb954f2f9f937068692e872a72074d20b322d

--- dali-extension/web-engine-chromium/tizen-web-engine-chromium.cpp
+++ dali-extension/web-engine-chromium/tizen-web-engine-chromium.cpp
@@ -88,6 +88,15 @@ void ExecuteCallback(Callback callback, Arg*& arg)
   }
 }
 
+template<typename Callback, typename Arg, typename... Args>
+void ExecuteCallback2(Callback callback, Arg*& arg, Args... args)
+{
+  if(callback)
+  {
+    callback(arg, args...);
+  }
+}
+
 template<typename Ret, typename Callback, typename... Args>
 Ret ExecuteCallbackReturn(Callback callback, Args... args)
 {
@@ -1436,14 +1445,16 @@ Eina_Bool TizenWebEngineChromium::OnGeolocationPermission(Evas_Object*, Ewk_Geol
 
 Eina_Bool TizenWebEngineChromium::OnUserMediaPermissonRequest(Evas_Object*, Ewk_User_Media_Permission_Request* request, void* data)
 {
-  auto                                                       pThis = static_cast<TizenWebEngineChromium*>(data);
-  std::unique_ptr<Dali::WebEngineUserMediaPermissionRequest> webUserMediaPermissionRequest(new TizenWebEngineUserMediaPermissionRequest(request));
-  DALI_LOG_RELEASE_INFO("#UserMediaPermissonRequest: pThis:%p, permission:%p\n", pThis, request);
-  std::string msg = ewk_user_media_permission_request_message_get(request);
+  auto pThis = static_cast<TizenWebEngineChromium*>(data);
+  pThis->mWebUserMediaPermissionRequest = new TizenWebEngineUserMediaPermissionRequest(request);
 
-  ExecuteCallback(pThis->mUserMediaPermissionRequestCallback, std::move(webUserMediaPermissionRequest), msg);
-  return msg.empty() ? false : true;
+  DALI_LOG_RELEASE_INFO("#UserMediaPermissonRequest: pThis:%p, request:%p\n", pThis, request);
+
+  std::string msg = ewk_user_media_permission_request_message_get(request);
+  ExecuteCallback2(pThis->mUserMediaPermissionRequestCallback, pThis->mWebUserMediaPermissionRequest, msg);
+  return msg.empty()? false: true;
 }
+
 
 } // namespace Plugin
 } // namespace Dali
