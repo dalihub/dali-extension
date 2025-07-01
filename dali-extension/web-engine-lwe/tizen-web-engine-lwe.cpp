a13a24c7e88ebb795e2b253ee83df944f677224d
Hosang Kim <hosang12.kim@samsung.com>
2025-07-01 15:26:24 +0900

    [Tizen] Initialize rendering surface when there is no rendering surface for LWE
    This reverts commit a56fdadbe34afc085ddbe7e20ae1fc5578e5024a.
    
    Change-Id: I87eb82ba33615a49922b6d22cf5986fa0b5454c1

--- dali-extension/web-engine-lwe/tizen-web-engine-lwe.cpp
+++ dali-extension/web-engine-lwe/tizen-web-engine-lwe.cpp
@@ -698,6 +698,10 @@ void TizenWebEngineLWE::TryRendering()
       InitRenderingSurface();
     }
   }
+  else
+  {
+    InitRenderingSurface();
+  }
 
   OnActive();
 
@@ -925,7 +929,7 @@ void TizenWebEngineLWE::InitRenderingSurface()
     return;
   }
 
-  mTbmQueue = tbm_surface_queue_create(gTbmSurfaceQueueLength, mWebContainer->Width(), mWebContainer->Height(), TBM_FORMAT_BGRA8888, TBM_BO_DEFAULT);
+  mTbmQueue = tbm_surface_queue_create(gTbmSurfaceQueueLength, std::max(mWebContainer->Width(), 1u), std::max(mWebContainer->Height(), 1u), TBM_FORMAT_BGRA8888, TBM_BO_DEFAULT);
 
   mEglSurface = eglCreateWindowSurface(mEglDisplay, mEglConfig, mTbmQueue, NULL);
   if(mEglSurface == EGL_NO_SURFACE)
