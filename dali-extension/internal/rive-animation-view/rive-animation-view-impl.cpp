29f6faecc2b6066b1e67d7542919a86edcce0bff
Richard Huang <r.huang@samsung.com>
2026-03-17 15:51:25 +0000

    Update include path after moving TypeRegistry to devel API
    Change-Id: Ic180f8a92fa18b67ed1da4f80646a787eeab5525

--- dali-extension/internal/rive-animation-view/rive-animation-view-impl.cpp
+++ dali-extension/internal/rive-animation-view/rive-animation-view-impl.cpp
@@ -22,12 +22,12 @@
 #include <dali-toolkit/devel-api/controls/control-devel.h>
 #include <dali/devel-api/adaptor-framework/window-devel.h>
 #include <dali/devel-api/common/stage.h>
+#include <dali/devel-api/object/type-registry-helper.h>
+#include <dali/devel-api/object/type-registry.h>
 #include <dali/devel-api/rendering/renderer-devel.h>
 #include <dali/integration-api/adaptor-framework/adaptor.h>
 #include <dali/integration-api/debug.h>
 #include <dali/integration-api/string-utils.h>
-#include <dali/public-api/object/type-registry-helper.h>
-#include <dali/public-api/object/type-registry.h>
 
 // INTERNAL INCLUDES
 #include <dali-extension/devel-api/rive-animation-view/rive-animation-view.h>
